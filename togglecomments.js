e2c14ab3bc762209b72beb0ce576cfac47bbf338
eet6646 <eet6646@gmail.com>
2017-04-20 22:11:35 -0400

    to spaces

--- togglecomments.js
+++ togglecomments.js
@@ -1,63 +1,63 @@
 
 var onCommentClick = function(commentId) {
-	var cURL = commentId.attr('href');
-	var parentDiv = commentId.parents('div:eq(1)');
-	var commentDivId = 'redditCommentToggle_' + parentDiv.attr('data-fullname');
+    var cURL = commentId.attr('href');
+    var parentDiv = commentId.parents('div:eq(1)');
+    var commentDivId = 'redditCommentToggle_' + parentDiv.attr('data-fullname');
 
-	var commentDiv = $('#'+commentDivId);
-	if(commentDiv.length == 0){
-		redditCommentToggle.requestPage(parentDiv, commentDivId, cURL);
-	}else{
-		commentDiv.slideToggle("slow");
-	}
+    var commentDiv = $('#'+commentDivId);
+    if(commentDiv.length == 0){
+        redditCommentToggle.requestPage(parentDiv, commentDivId, cURL);
+    }else{
+        commentDiv.slideToggle("slow");
+    }
 }
 
 
 $("a.comments").click(function() {
-	onCommentClick($(this));
-	return false;
+    onCommentClick($(this));
+    return false;
 }).addClass("commenttoggle");
 
 
 function nodeInsertedCallback(event) {
 
-	if(event.relatedNode.className.indexOf("sitetable") > -1){
-		console.log("here");
-		$(event.relatedNode).find('a.comments:not(.commenttoggle)').each(function(i) {
-			$(this).click(function() {
-					onCommentClick($(this));
-					return false;
-			});
-			$(this).addClass("commenttoggle");
-		});
-	}
+    if(event.relatedNode.className.indexOf("sitetable") > -1){
+        console.log("here");
+        $(event.relatedNode).find('a.comments:not(.commenttoggle)').each(function(i) {
+            $(this).click(function() {
+                    onCommentClick($(this));
+                    return false;
+            });
+            $(this).addClass("commenttoggle");
+        });
+    }
 };
 
 document.body.addEventListener('DOMNodeInserted', nodeInsertedCallback);
 
 var redditCommentToggle = {
-	requestPage: function(parentDiv, commentDivId, url) {
+    requestPage: function(parentDiv, commentDivId, url) {
 
-		$.get(url,function(source) {
+        $.get(url,function(source) {
 
-			var loadedComments = $(source).find('div[id^="siteTable_t3_"]');
-			var commentDiv = $("<div>").html(loadedComments);
-			commentDiv.attr('id', commentDivId)
+            var loadedComments = $(source).find('div[id^="siteTable_t3_"]');
+            var commentDiv = $("<div>").html(loadedComments);
+            commentDiv.attr('id', commentDivId)
 
-			var scrollUpFunc = function() {
-				$('html,body').animate({ scrollTop: parentDiv.offset().top }, 'slow');
-			}
+            var scrollUpFunc = function() {
+                $('html,body').animate({ scrollTop: parentDiv.offset().top }, 'slow');
+            }
 
-			var scrollUp = $(" <a>(back up)</a>").click(scrollUpFunc);
-			var hideAll = $(" <a>(hide all)</a>").click(function() {
-				commentDiv.slideToggle("slow");
-				scrollUpFunc();
-				return false;
-			});
-			loadedComments.find("p.tagline").append(" -- ").append(scrollUp);
-			loadedComments.find("p.tagline").append(" - ").append(hideAll);
+            var scrollUp = $(" <a>(back up)</a>").click(scrollUpFunc);
+            var hideAll = $(" <a>(hide all)</a>").click(function() {
+                commentDiv.slideToggle("slow");
+                scrollUpFunc();
+                return false;
+            });
+            loadedComments.find("p.tagline").append(" -- ").append(scrollUp);
+            loadedComments.find("p.tagline").append(" - ").append(hideAll);
 
-			parentDiv.append(commentDiv).hide().slideDown("slow");
-		});
-	}
+            parentDiv.append(commentDiv).hide().slideDown("slow");
+        });
+    }
 }
